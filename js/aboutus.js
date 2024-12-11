efae89902cbffba6a7a6ee005039ab6e0ceb4a80
chalitha11 <pilapiti2@coventry.ac.uk>
2024-12-12 03:02:38 +0530

    Delete js directory

--- js/aboutus.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("scroll", () => {
-    document.querySelector(".background").style.backgroundSize = `${window.scrollY * 1.8 + 1600}px`;
-    document.querySelector(".background h1").style.opacity = `${(-window.scrollY + 300) * 0.004}`;
-  });
